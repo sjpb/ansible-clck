d4fcc9021bbf7323ff584770cd120d1df5910ad1
Steve Brasier <steveb@stackhpc.com>
2019-05-29 08:56:54 +0000

    fix environment

--- run_clck.sh
+++ run_clck.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 # load intel mpi environment
-source `find /opt/intel -name mpivars.sh`
+source `find /opt/intel/ -name mpivars.sh`
+# load intel mlk enviroment
+source `find /opt/intel/ -name mklvars.sh`
 # load clck environment
 source `find /opt/intel/ -name clckvars.sh`
+
 # run clck with healthcheck modules
 clck -f clck_nodes         
