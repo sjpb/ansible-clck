4a7150c482605c9f2909dc3d4439a8966a8d4eb6
Steve Brasier <steveb@stackhpc.com>
2019-05-29 14:49:40 +0000

    add specific intel versions to fix incompatibilities

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Work-in-progress playbook to install/use intel cluster checker (clck - https://software.intel.com/en-us/intel-cluster-checker) on a p3-appliances (https://github.com/stackhpc/p3-appliances) -type openhpc cluster
+
+Useful docs:
+https://software.intel.com/en-us/articles/installing-intel-free-libs-and-python-yum-repo
+https://software.intel.com/en-us/get-started-with-mpi-for-linux
+https://software.intel.com/en-us/mkl-linux-developer-guide-running-the-intel-distribution-for-linpack-benchmark
+https://software.intel.com/en-us/cluster-checker-user-guide-installation
--- run_clck.sh
+++ run_clck.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
-# load intel mpi environment
-source `find /opt/intel/ -name mpivars.sh`
-# load intel mlk enviroment
-source `find /opt/intel/ -name mklvars.sh`
-# load clck environment
-source `find /opt/intel/ -name clckvars.sh`
+# load intel environments:
+source /opt/intel/compilers_and_libraries_2019.4.243/linux/bin/compilervars.sh intel64
+source /opt/intel/compilers_and_libraries_2019.4.243/linux/mkl/bin/mklvars.sh intel64
+source /opt/intel/compilers_and_libraries_2019.4.243/linux/mpi/intel64/bin/mpivars.sh
+
+# load clck environment:
+source /opt/intel/clck/2019.3.5/bin/clckvars.sh
 
 # run clck with healthcheck modules
-clck -f clck_nodes         
+#clck -f clck_nodes -l debug &> clck_debug.log
+clck -f clck_nodes -l debug -F hpl_cluster_performance &> clck_debug.log         
