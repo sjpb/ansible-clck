a7bff10e8707fe02aa65f89bd43c66eec3cde2a8
Steve Brasier <steveb@stackhpc.com>
2019-05-31 08:51:50 +0000

    update versions

--- run_clck.sh
+++ run_clck.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/bash
 
 # load intel environments:
-source /opt/intel/compilers_and_libraries_2019.4.243/linux/bin/compilervars.sh intel64
-source /opt/intel/compilers_and_libraries_2019.4.243/linux/mkl/bin/mklvars.sh intel64
-source /opt/intel/compilers_and_libraries_2019.4.243/linux/mpi/intel64/bin/mpivars.sh
+#source /opt/intel/compilers_and_libraries_2019.4.243/linux/bin/compilervars.sh intel64
+#source /opt/intel/compilers_and_libraries_2019.4.243/linux/mkl/bin/mklvars.sh intel64
+source  /opt/intel/compilers_and_libraries_2018.5.274/linux/mpi/intel64/bin/mpivars.sh
 
-# load clck environment:
 source /opt/intel/clck/2019.3.5/bin/clckvars.sh
 
-# run clck with healthcheck modules
-#clck -f clck_nodes -l debug &> clck_debug.log
-clck -f clck_nodes -l debug -F hpl_cluster_performance &> clck_debug.log         
+# run clck
+clck -f clck_nodes -l debug &> clck_debug.log
+#clck -f clck_nodes -l debug -F hpl_cluster_performance &> clck_debug.log         
